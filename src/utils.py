4b202a6799c576786511b71e1967384c54daf548
nrobotics-dev <dominik@nrobotics.com>
2022-10-26 12:33:42 +0200

    show images done

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 def load_image(path):
     file = open(path, "rb").read()
     image = []
@@ -18,3 +21,21 @@ def convert_image(img):
     img_normed = (gray - np.min(gray)) / (np.max(gray) - np.min(gray))
 
     return img_normed
+
+def show_images(grid_size: int, path_to_images: str) -> None:
+    labels = [l for l in os.listdir(path_to_images) if l.isdigit()]
+    images = [(path_to_images+label+"/"+f, label)  for label in labels for f in os.listdir(path_to_images+label)]
+    columns, rows = grid_size, grid_size
+    figure = plt.figure()
+    figure.set_figheight(20)
+    figure.set_figwidth(20)
+    for idx in range(grid_size * grid_size):
+        random_idx = np.random.randint(low=0, high=len(images))
+        image_to_load = images[random_idx][0]
+        label = images[random_idx][1]
+        image_to_show = load_image(image_to_load)
+        axes = figure.add_subplot(columns, rows, idx + 1, )
+        axes.imshow(image_to_show)
+        axes.set_title(label=label)
+        axes.axis('off')
+    plt.show()
