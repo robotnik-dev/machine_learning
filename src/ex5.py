35b1cae82a284280a68888fa4f73bd59b2f0cb23
Robotnik <85544501+robotnik-dev@users.noreply.github.com>
2022-11-21 10:42:37 +0100

    Exercise 5

--- src/ex5.py
+++ src/ex5.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import numpy as np
 import os
 from utils import load_image, convert_image
-import matplotlib; matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import torch
 
 
@@ -21,8 +22,7 @@ class ImageDataset():
         image = np.array(image)
         image = convert_image(image)
         if self.tranform:
-            pass
-            #image = self.tranform(image)
+            image = self.tranform(image)
         label = int(lbl)
         return image, label
     
@@ -32,16 +32,16 @@ class ImageDataset():
     def show_batch(self, b):
         img,lbl = self.__getitem__(0)
         size = int(np.sqrt(b))
-        figure = matplotlib.pyplot.figure(figsize=(2*size, 2*size))
+        figure = plt.figure(figsize=(2*size, 2*size))
         cols, rows = 2*size, size//2
         for i in range(1, cols * rows + 1):
             sample_idx = torch.randint(len(self), size=(1,)).item()
             img, label = self.__getitem__(sample_idx)
             figure.add_subplot(rows, cols, i)
-            matplotlib.pyplot.title(str(label))
-            matplotlib.pyplot.axis("off")
-            matplotlib.pyplot.imshow(img.squeeze(), cmap="gray")
-        matplotlib.pyplot.show()
+            plt.title(str(label))
+            plt.axis("off")
+            plt.imshow(img.squeeze(), cmap="gray")
+        plt.show()
 
 
 def gaussian(a):
@@ -51,12 +51,16 @@ def gaussian(a):
     a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 1, a)
     return a/norm
 
+def shake(img):
+    pass
 
 def main():
     train_path = "./data/MYON/train/"
     train_dataset = ImageDataset(train_path, transform=gaussian)
 
-    matplotlib.pyplot.imshow(train_dataset[0][0], cmap="gray")
+    plt.imshow(train_dataset[0][0], cmap="gray")
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
