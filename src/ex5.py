6a04db994a997b4814ca4552d023fb1cb56fe713
robotnik <robotnik.games@gmail.com>
2022-11-23 13:45:20 +0100

    Aufgabe 5

--- src/ex5.py
+++ src/ex5.py
@@ -1,61 +1,27 @@
 #!/usr/bin/env python3
 import numpy as np
 import os
-from utils import load_image, convert_image
+from utils import load_image, convert_image, ImageDataset, gaussian, shift_x, shift_y, randomise_colors, flip
 import matplotlib.pyplot as plt
 import torch
 
-
-class ImageDataset():
-    def __init__(self, base_path, transform=None):
-        self.img_paths = self.load_paths(base_path)
-        self.tranform = transform
-
-    def load_paths(self, p):
-        labels = [l for l in os.listdir(p) if l.isdigit()]
-        images = [(p+label+"/"+f, label)  for label in labels for f in os.listdir(p+label)]
-        return images
-    
-    def __getitem__(self, idx):
-        img_p, lbl = self.img_paths[idx]
-        image = load_image(img_p)
-        image = np.array(image)
-        image = convert_image(image)
-        if self.tranform:
-            image = self.tranform(image)
-        label = int(lbl)
-        return image, label
-    
-    def __len__(self):
-        return len(self.img_objects)
-
-    def show_batch(self, b):
-        img,lbl = self.__getitem__(0)
-        size = int(np.sqrt(b))
-        figure = plt.figure(figsize=(2*size, 2*size))
-        cols, rows = 2*size, size//2
-        for i in range(1, cols * rows + 1):
-            sample_idx = torch.randint(len(self), size=(1,)).item()
-            img, label = self.__getitem__(sample_idx)
-            figure.add_subplot(rows, cols, i)
-            plt.title(str(label))
-            plt.axis("off")
-            plt.imshow(img.squeeze(), cmap="gray")
-        plt.show()
-
-
-def gaussian(a):
-    kernel = np.array([1, 2, 1]) # Here you would insert your actual kernel of any size
-    norm = np.sum(np.outer(kernel, kernel))
-    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, a)
-    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 1, a)
-    return a/norm
+def transforms(img):
+    img = flip(img)
+    img = gaussian(img)
+    img = shift_x(img)
+    img = shift_y(img)
+    return img
 
 def main():
     train_path = "./data/MYON/train/"
-    train_dataset = ImageDataset(train_path, transform=gaussian)
-
+    train_dataset = ImageDataset(train_path, transform=transforms)
+    # plt.subplot(1,3,1)
     plt.imshow(train_dataset[0][0], cmap="gray")
+    # plt.subplot(1,3,2)
+    # plt.imshow(train_dataset2[0][0], cmap="gray")
+    # plt.subplot(1,3,3)
+    # plt.imshow(train_dataset3[0][0], cmap="gray")
+
     plt.show()
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import torch
 
 def load_image(path):
     file = open(path, "rb").read()
@@ -42,11 +43,73 @@ def show_images(grid_size: int, path_to_images: str) -> None:
     plt.show()
 
 
-class ImageDataset:
-    def __init__(self, base_path: str) -> None:
-        self.base_path = base_path
+class ImageDataset():
+    def __init__(self, base_path, transform=None):
+        self.img_paths = self.load_paths(base_path)
+        self.tranform = transform
+
+    def load_paths(self, p):
+        labels = [l for l in os.listdir(p) if l.isdigit()]
+        images = [(p+label+"/"+f, label)  for label in labels for f in os.listdir(p+label)]
+        return images
     
-    def get_img_paths(self):
-        img_paths = [[(img_path, label) for img_path in os.listdir(self.base_path+label)]
-                     for label in os.listdir(self.base_path)]
-        return img_paths
+    def __getitem__(self, idx):
+        img_p, lbl = self.img_paths[idx]
+        image = load_image(img_p)
+        image = np.array(image)
+        image = convert_image(image)
+        if self.tranform:
+            image = self.tranform(image)
+        label = int(lbl)
+        return image, label
+    
+    def __len__(self):
+        return len(self.img_objects)
+
+    def show_batch(self, b):
+        img,lbl = self.__getitem__(0)
+        size = int(np.sqrt(b))
+        figure = plt.figure(figsize=(2*size, 2*size))
+        cols, rows = 2*size, size//2
+        for i in range(1, cols * rows + 1):
+            sample_idx = torch.randint(len(self), size=(1,)).item()
+            img, label = self.__getitem__(sample_idx)
+            figure.add_subplot(rows, cols, i)
+            plt.title(str(label))
+            plt.axis("off")
+            plt.imshow(img.squeeze(), cmap="gray")
+        plt.show()
+
+
+def gaussian(a):
+    kernel = np.array([1, 2, 1]) # Here you would insert your actual kernel of any size
+    norm = np.sum(np.outer(kernel, kernel))
+    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, a)
+    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 1, a)
+    return a/norm
+
+def shift_x(a):
+    shift = np.random.randint(5)
+    kernel = np.array([1, 2, 1]) # Here you would insert your actual kernel of any size
+    norm = np.sum(np.outer(kernel, kernel))
+    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, a)
+    a = np.apply_along_axis(lambda x: np.convolve(np.roll(x, shift), kernel, mode='same'), 1, a)
+    return a/norm
+
+def shift_y(a):
+    shift = np.random.randint(5)
+    kernel = np.array([1, 2, 1]) # Here you would insert your actual kernel of any size
+    norm = np.sum(np.outer(kernel, kernel))
+    a = np.apply_along_axis(lambda x: np.convolve(np.roll(x, shift), kernel, mode='same'), 0, a)
+    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 1, a)
+    return a/norm
+
+def randomise_colors(a):
+    kernel = np.array([1, 2, 1]) # Here you would insert your actual kernel of any size
+    norm = np.sum(np.outer(kernel, kernel))
+    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 0, a)
+    a = np.apply_along_axis(lambda x: np.convolve(x, kernel, mode='same'), 1, a)
+    return a/norm
+
+def flip(img):
+    return img[:, -1::-1]
